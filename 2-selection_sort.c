0361628fca0fc1d23c167972e5efc760811f8a1b
Wilson Valer <nederig21@gmail.com>
2022-08-25 23:14:34 -0500

    Write a function that sorts an array of integers in ascending order using the Selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+/**
+ * selection_sort - is an in-place comparison sorting algorithm.
+ * @array: Array of number to sort.
+ * @size: Size of the array.
+ *
+ * Return: Void function ( Nothing).
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, temp, index, j;
+
+	if (size < 2)
+		return;
+	for(i = 0; i < size; i++)
+	{
+		index = i;
+		for(j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[index])
+				index = j;
+		}
+		if (index != j)
+		{
+			temp = array[i];
+			array[i] = array[index];
+			array[index] = temp;
+			print_array(array, size);
+		}
+	}
+}
